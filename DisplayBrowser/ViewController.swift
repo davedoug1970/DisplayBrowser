1b4c702494fdfe29f995105608963d8811cbace8
David Douglas <dave@softwaresmoothie.com>
2022-07-28 15:51:50 -0500

    initial commit

--- DisplayBrowser/ViewController.swift
+++ DisplayBrowser/ViewController.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let myURL = URL(string:"https://www.apple.com")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
     }
-
 
 }
 
